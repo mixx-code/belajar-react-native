296cf3f8b1b378d71ceac5cbee7d6d27dfe3ddab
mixx-code <rfebriansyah385@gmail.com>
2024-07-04 21:50:30 +0700

    rubah APP

--- App.js
+++ App.js
@@ -32,15 +32,7 @@ const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
+      
       <Text
         style={[
           styles.sectionDescription,
